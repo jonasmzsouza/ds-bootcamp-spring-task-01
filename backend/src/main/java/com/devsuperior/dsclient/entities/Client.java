79cb5f4bb307455a70caf1a82f5c7e72507863a7
Jonas Souza <jonasmzsouza@gmail.com>
2022-03-22 20:57:02 -0300

    Client repository, service, resource, H2 database, database seeding

--- backend/src/main/java/com/devsuperior/dsclient/entities/Client.java
+++ backend/src/main/java/com/devsuperior/dsclient/entities/Client.java
@@ -4,13 +4,26 @@ import java.io.Serializable;
 import java.time.Instant;
 import java.util.Objects;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tb_client")
 public class Client implements Serializable {
 	private static final long serialVersionUID = 1L;
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
 	private String cpf;
 	private Double income;
+	
+	@Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
 	private Instant birthDate;
 	private Integer children;
 
@@ -18,7 +31,6 @@ public class Client implements Serializable {
 	}
 
 	public Client(Long id, String name, String cpf, Double income, Instant birthDate, Integer children) {
-		super();
 		this.id = id;
 		this.name = name;
 		this.cpf = cpf;
--- backend/src/main/java/com/devsuperior/dsclient/resources/ClientResource.java
+++ backend/src/main/java/com/devsuperior/dsclient/resources/ClientResource.java
@@ -1,25 +1,26 @@
 package com.devsuperior.dsclient.resources;
 
-import java.time.Instant;
-import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.devsuperior.dsclient.entities.Client;
+import com.devsuperior.dsclient.services.ClientService;
 
 @RestController
 @RequestMapping(value = "/clients")
 public class ClientResource {
 
+	@Autowired
+	private ClientService service;
+
 	@GetMapping
 	public ResponseEntity<List<Client>> findAll() {
-		List<Client> list = new ArrayList<>();
-		list.add(new Client(1L, "Antônio Bastos", "111111111-11", 6000.0, Instant.parse("1991-01-01T11:11:11Z"), 1));
-		list.add(new Client(2L, "Camila Duarte", "222222222-22", 8000.0, Instant.parse("1992-02-02T22:22:22Z"), 2));
+		List<Client> list = service.findAll();
 		return ResponseEntity.ok().body(list);
 	}
 
