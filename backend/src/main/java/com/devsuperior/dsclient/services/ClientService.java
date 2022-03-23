0d03e82d853702414d80400ba714210e4ab69c6c
Jonas Souza <jonasmzsouza@gmail.com>
2022-03-22 22:00:36 -0300

    Post new client

--- backend/src/main/java/com/devsuperior/dsclient/services/ClientService.java
+++ backend/src/main/java/com/devsuperior/dsclient/services/ClientService.java
@@ -24,11 +24,28 @@ public class ClientService {
 		List<Client> list = repository.findAll();
 		return list.stream().map(x -> new ClientDTO(x)).collect(Collectors.toList());
 	}
-	
+
+	@Transactional(readOnly = true)
 	public ClientDTO findById(Long id) {
 		Optional<Client> obj = repository.findById(id);
 		Client entity = obj.orElseThrow(() -> new ServiceNotFoundException("Entity not found"));
 		return new ClientDTO(entity);
+	}
+
+	@Transactional(readOnly = true)
+	public ClientDTO insert(ClientDTO dto) {
+		Client entity = new Client();
+		copyDtoToEntity(dto, entity);
+		entity = repository.save(entity);
+		return new ClientDTO(entity);
+	}
+
+	private void copyDtoToEntity(ClientDTO dto, Client entity) {
+		entity.setName(dto.getName());
+		entity.setCpf(dto.getCpf());
+		entity.setIncome(dto.getIncome());
+		entity.setBirthDate(dto.getBirthDate());
+		entity.setChildren(dto.getChildren());
 	}
 
 }
